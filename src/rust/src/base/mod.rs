bfd2b63c10d0c288a1895ef4ab5216fe40932427
Marco Fura <mfura@kataricorp.com>
2024-08-02 20:13:40 -0400

    feat: add rust

---
+++ src/rust/src/base/mod.rs
@@ -0,0 +1,1 @@
+pub mod hola_mundo;
---
+++ src/rust/src/main.rs
@@ -0,0 +1,15 @@
+mod base; // Importa el módulo base
+
+use base::hola_mundo::init;
+
+fn main() {
+    println!("----------------------");
+    println!("RUST");
+    println!("----------------------");
+    let start_time = std::time::Instant::now();
+    init();
+    let end_time = std::time::Instant::now();
+    let duration = end_time.duration_since(start_time);
+    let duration_ms = duration.as_millis();
+    println!("Tiempo de Ejecución: {:?}", duration_ms);
+}
